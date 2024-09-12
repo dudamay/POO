74c8374633f5e34d80bf6594344461b1996a4e91
Maysa <maysaeduarda1006@gmail.com>
2024-09-12 08:41:04 -0400

    add atributos POO

---
+++ 02-atributos.js
@@ -0,0 +1,8 @@
+class Pessoa{
+    constructor(nome,idade,altura,peso) { //Atributos // Inicialização dos atributos
+        this.nome=none;
+        this.idade=idade;
+        this.altura=altura;
+        this.peso=peso;
+    }
+}
