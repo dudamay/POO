162ae32dd00316942e726de803d8dd1fa72cdd2a
Maysa <maysaeduarda1006@gmail.com>
2024-09-19 08:26:07 -0400

    add objeto

--- 07-heranca.js
+++ 07-heranca.js
@@ -39,3 +39,17 @@ class Professor extends Funcionario {
       
     }
 }
+
+// Criando objetos aluno,diretor e professor
+
+const aluno = new Estudante("João", 16, 1.78, 70, "Programação de Jogos Digitais");
+const diretor = new Diretor("Sérgio", 45, 1.65, 65, 3000);
+const professor = new Professor("Wanderson", 48, 1.65, 70, 1000);
+
+// Mostrando no console a Saída
+
+console.log(aluno);//Saída: Estudante{nome: "João", idade:16, altura:1.7,peso:70,curso:"Programação de Jogos Digitais"}
+console.log(diretor);//Saída: Diretor{nome: "Sérgio", idade:45, altura:1.65,peso:65,salario:3000}
+console.log(professor);//Saída: Professor{nome: "Wanderson", idade:48, altura:1.65,peso:70,salario:1000}
+
+
