c079b599ba3fa870fc30a9f1a687afa7f1399a6d
Maysa <maysaeduarda1006@gmail.com>
2024-10-01 08:19:21 -0400

    add encapsulamento getters e setters

---
+++ 15-encapsulamento-getters-setters.js
@@ -0,0 +1,44 @@
+// Classe base Personagem
+class Personagem {
+    // Encapsulando os atributos com #, tornando-os privados
+    #nome;
+    #classe;
+    #nivel;
+    #vida;
+    #mana;
+    // Método Construtor
+    constructor(nome, classe, nivel, vida, mana) {
+        this.#nome = nome;
+        this.#classe = classe;
+        this.#nivel = nivel;
+        this.#vida = vida;
+        this.#mana = mana;
+    }
+    // Métodos  Getters e Setters
+    // Métodos Getters e Setters para o atributo nome
+    get nome() {
+        return this.#nome;
+    }
+    set nome(nome) {
+        this.#nome = nome;
+    }
+    // Métodos Getters e Setters para o atributo classe
+    get classe() {
+        return this.#classe;
+    }
+    // Métodos Getter e Setter para o atributo nìvel 
+    get nivel() {
+        return this.#nivel;
+    }
+    // Métodos Getter e Setter para o atributo vida
+    get vida() {
+        return this.#vida;
+    }
+    // Métodos Getter e Setter para o atributo mana
+    get mana() {
+        return this.#mana;
+    }
+    set mana(mana){
+        this.#mana = mana;
+    }
+}
